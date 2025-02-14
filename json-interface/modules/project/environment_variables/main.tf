250e0c2f9e497bf8a9c78640e98480f179882c25
trouze <tyler@tylerrouze.com>
2025-02-14 14:31:00 -0600

    add projects:

--- json-interface/modules/project/environment_variables/main.tf
+++ json-interface/modules/project/environment_variables/main.tf
@@ -16,3 +16,17 @@ resource "dbtcloud_environment_variable" "environment_variables" {
   project_id = var.project_id
   environment_values = each.value.environment_values
 }
+
+resource "dbtcloud_environment_variable" "environment_variables" {
+  for_each = {
+    for env_var in var.environment_variables :
+    env_var.name => env_var
+  }
+
+  name           = each.value.name
+  project_id     = var.project_id
+  environment_values = {
+    for key, value in each.value.environment_values :
+    key => contains(value, "secret_") ? lookup(var.token_map, value, null) : value
+  }
+}
